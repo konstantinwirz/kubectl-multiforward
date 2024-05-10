eb4fffb28f5a563a75c75633ee42dff807916091
Wirz, Konstantin (external) <konstantin.wirz@protonmail.com>
2024-05-10 19:48:05 +0200

    Use cobra for flag parsing. Introduce reporting channel.

--- forwarder.go
+++ forwarder.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/portforward"
 	"k8s.io/client-go/transport/spdy"
@@ -45,7 +46,14 @@ func NewForwardResultWithError(poder Poder, err error) ForwardResult {
 	}
 }
 
-func (f Forwarder) Start(wg *sync.WaitGroup, poder Poder, resultsChan chan<- ForwardResult, stopChan <-chan struct{}) error {
+// forwardSingle establishes a single port forwarding connection for a given Poder.
+func (f Forwarder) forwardSingle(
+	wg *sync.WaitGroup,
+	poder Poder,
+	resultsChan chan<- ForwardResult,
+	stopChan <-chan struct{},
+	reportChan chan<- Report,
+) error {
 	pod, err := poder.Pod()
 	if err != nil {
 		return fmt.Errorf("couldn't establish port forwarding -> %s", err)
@@ -68,6 +76,16 @@ func (f Forwarder) Start(wg *sync.WaitGroup, poder Poder, resultsChan chan<- For
 	readyChan := make(chan struct{}, 1)
 	out, errOut := new(bytes.Buffer), new(bytes.Buffer)
 
+	oldRuntimeErrorHandlers := runtime.ErrorHandlers
+	runtime.ErrorHandlers = []func(error){
+		func(err error) {
+			reportChan <- NewReport(SeverityError, poder, err.Error())
+		},
+	}
+	if len(oldRuntimeErrorHandlers) >= 2 {
+		runtime.ErrorHandlers = append(runtime.ErrorHandlers, oldRuntimeErrorHandlers[1])
+	}
+
 	forwarder, err := portforward.New(dialer, poder.Ports(), stopChan, readyChan, out, errOut)
 	if err != nil {
 		return fmt.Errorf("error creating port forwarder: %w", err)
@@ -78,22 +96,21 @@ func (f Forwarder) Start(wg *sync.WaitGroup, poder Poder, resultsChan chan<- For
 		<-readyChan
 
 		if len(errOut.String()) != 0 {
-			fmt.Printf("[%s/%s] [stderr]: %s\n", poder.Namespace(), pod, errOut)
+			reportChan <- NewReport(SeverityError, poder, strings.TrimSpace(strings.ReplaceAll(out.String(), "\n", "; ")))
 		}
 
 		if len(out.String()) != 0 {
-			fmt.Printf("[%s/%s] [stdout]: %s\n", poder.Namespace(), pod, strings.Replace(out.String(), "\n", "; ", -1))
+			reportChan <- NewReport(SeverityInfo, poder, strings.TrimSpace(strings.ReplaceAll(out.String(), "\n", "; ")))
 		}
-
 	}()
 
 	go func() {
 		defer wg.Done()
 
-		fmt.Printf("[%s/%s] port forwarding established\n", poder.Namespace(), pod)
+		reportChan <- NewReport(SeverityDebug, poder, "establishing port forwarding for %s ...", pod)
 
 		if err = forwarder.ForwardPorts(); err != nil {
-			fmt.Printf("[%s/%s] error forwarding ports: %s\n", poder.Namespace(), pod, err.Error())
+			reportChan <- NewReport(SeverityError, poder, "error forwarding ports: %s", err.Error())
 			resultsChan <- NewForwardResultWithError(poder, err)
 		}
 		resultsChan <- NewForwardResult(poder)
@@ -102,7 +119,38 @@ func (f Forwarder) Start(wg *sync.WaitGroup, poder Poder, resultsChan chan<- For
 	return nil
 }
 
-func (f Forwarder) StartMany(poders []Poder, stopChan <-chan struct{}) (<-chan struct{}, error) {
+func (f Forwarder) forwardSingleInALoop(
+	wg *sync.WaitGroup,
+	poder Poder,
+	resultsChan chan<- ForwardResult,
+	stopChan <-chan struct{},
+	reportChan chan<- Report,
+) {
+	t := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case <-stopChan:
+			reportChan <- NewReport(SeverityInfo, poder, "received stop signal, no more attempts to restart forwarder")
+			return
+		case <-t.C:
+			reportChan <- NewReport(SeverityTrace, poder, "trying to restart forwarder...")
+			if err := f.forwardSingle(wg, poder, resultsChan, stopChan, reportChan); err == nil {
+				t.Stop()
+				wg.Add(1)
+				reportChan <- NewReport(SeverityInfo, poder, "restarted forwarder...")
+				return
+			}
+			// just do it again until err == nil
+		}
+	}
+}
+
+// Forward establishes port forwarding for all given Poder instances.
+func (f Forwarder) Forward(
+	poders []Poder,
+	stopChan <-chan struct{},
+	reportChan chan<- Report,
+) (<-chan struct{}, error) {
 	resultsChan := make(chan ForwardResult, len(poders))
 	targetStopChan := make(chan struct{})
 	doneChan := make(chan struct{})
@@ -117,38 +165,13 @@ func (f Forwarder) StartMany(poders []Poder, stopChan <-chan struct{}) (<-chan s
 			select {
 			case result := <-resultsChan:
 				if result.IsError() {
-					// restart the forwarder
-					err := f.Start(&wg, result.Source, resultsChan, targetStopChan)
-					if err != nil {
-						fmt.Printf("error restarting forwarder, try it again in a loop -> %s\n", err.Error())
-
-						go func() {
-							t := time.NewTicker(5 * time.Second)
-							for {
-								select {
-								case <-stopChan:
-									fmt.Println("received stop signal, no more attempts to restart forwarder")
-									return
-								case <-t.C:
-									if err := f.Start(&wg, result.Source, resultsChan, targetStopChan); err == nil {
-										t.Stop()
-										wg.Add(1)
-										return
-									}
-									// just do it again until err == nil
-								}
-							}
-						}()
-
-					} else {
-						wg.Add(1)
-					}
+					go f.forwardSingleInALoop(&wg, result.Source, resultsChan, targetStopChan, reportChan)
 				}
 			case <-stopChan:
-				fmt.Printf("received stop signal, stopping all forwarders...")
+				reportChan <- NewReport(SeverityInfo, nil, "received stop signal, stopping all forwarders...")
 				close(targetStopChan)
 				wg.Wait()
-				fmt.Printf("DONE\n")
+				reportChan <- NewReport(SeverityInfo, nil, "all forwarders stopped")
 				break loop
 			}
 		}
@@ -156,7 +179,7 @@ func (f Forwarder) StartMany(poders []Poder, stopChan <-chan struct{}) (<-chan s
 
 	for _, poder := range poders {
 		wg.Add(1)
-		if err := f.Start(&wg, poder, resultsChan, targetStopChan); err != nil {
+		if err := f.forwardSingle(&wg, poder, resultsChan, targetStopChan, reportChan); err != nil {
 			return doneChan, fmt.Errorf("error starting forwarder: %w", err)
 		}
 	}
